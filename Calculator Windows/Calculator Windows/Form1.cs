fac0b50a59c88e8e32cc7847dfddd17a968c1d89
Roberto Morales <xd366@hotmail.com>
2017-05-22 15:15:02 -0700

    Calculator basic design set. Digits can be entered.

--- Calculator Windows/Calculator Windows/Form1.cs
+++ Calculator Windows/Calculator Windows/Form1.cs
@@ -10,11 +10,30 @@ using System.Windows.Forms;
 
 namespace Calculator_Windows
 {
-    public partial class Form1 : Form
+    public partial class Calculator : Form
     {
-        public Form1()
+
+        Double resetValue = 0;
+        String operationPerformed = "";
+
+
+        public Calculator()
         {
             InitializeComponent();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+                textBox1.Clear();
+
+            Button button = (Button)sender;
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
         }
     }
 }
